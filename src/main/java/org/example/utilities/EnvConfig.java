cc1e183e3fbe7e58498a07b5501585d197b83e2d
TPiotrKozieja <piotrusiekmaster@gmail.com>
2024-08-29 13:23:09 +0200

    Fixing duplicate files

--- src/main/java/org/example/utilities/EnvConfig.java
+++ src/main/java/org/example/utilities/EnvConfig.java
@@ -3,10 +3,7 @@ package org.example.utilities;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class EnvConfig {
-    private static Dotenv dotenv = Dotenv.configure()
-            .filename("data.env")
-            .directory("C:\\Users\\piotr.kozieja\\Desktop\\SpringBoot\\Salesforce-Cucumber-Demountitled")
-            .load();
+    private static Dotenv dotenv = Dotenv.load();
 
     public static String get(String key) {
         return dotenv.get(key);
